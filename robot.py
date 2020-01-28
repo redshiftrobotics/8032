dbe6279e41e3e73e193d5d09f4c02d7deaffdb1d
Kavi Dey <kaviasher@gmail.com>
2020-01-27 16:28:26 -0800

    fixed broken code

--- robot.py
+++ robot.py
@@ -6,9 +6,6 @@ from robotpy_ext.control.button_debouncer import ButtonDebouncer
 from state import State
 from state import DRIVE_FORWARD_TWO_SEC, FREEZE, TANK_DRIVE_NORMAL
 
-# Broken code because julian asked for it
-asdfladflasdlfkasdf. !()()**&%$!SGLJKSB self. robot.  *&
-
 class Robot(wpilib.TimedRobot):
     def threshhold(self, value, limit):
          if (abs(value) < limit):
