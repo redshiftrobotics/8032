36b7ab32cf64586967af6cc7896950322212629f
Kavi Dey <kaviasher@gmail.com>
2020-01-31 15:59:47 -0800

    updated code for robotpy 2020

--- robot.py
+++ robot.py
@@ -3,28 +3,10 @@ import wpilib
 from wpilib.drive import DifferentialDrive
 from networktables import NetworkTables
 from robotpy_ext.control.button_debouncer import ButtonDebouncer
-from state import State
-from state import DRIVE_FORWARD_TWO_SEC, FREEZE, TANK_DRIVE_NORMAL
 
-from ctre import WPI_TalonSRX, NeutralMode
-
-# ENCODER_COUNT = 2
+from ctre import WPI_TalonSRX, ControlMode, NeutralMode, FeedbackDevice
 
 class Robot(wpilib.TimedRobot):
-    # ENCODER_PULSE_PER_REV = 1024
-    # WHEEL_DIAMETER = 0.5
-    # 
-    # talon_id_set = [
-    #     ('Front Left', 5, False),
-    #     ('Front Right', 1, False),
-    #     ('Back Left', 4, False),
-    #     ('Back Right', 15, False)
-    # ]
-    # 
-    # encoder_value_sets = []  # tuples of (home_pos, min_pos, max_pos)
-    # last_encoder_values = []
-    # talon_sweeping = []
-    
     def threshhold(self, value, limit):
          if (abs(value) < limit):
              return 0
@@ -32,17 +14,8 @@ class Robot(wpilib.TimedRobot):
              return round(value, 2)
 
     def robotInit(self):
-        #: Which PID slot to pull gains from. Starting 2018, you can choose from
-        #: 0,1,2 or 3. Only the first two (0,1) are visible in web-based
-        #: configuration.
         self.kSlotIdx = 0
-
-        #: Talon SRX/ Victor SPX will supported multiple (cascaded) PID loops. For
-        #: now we just want the primary one.
         self.kPIDLoopIdx = 0
-
-        #: set to zero to skip waiting for confirmation, set to nonzero to wait and
-        #: report to DS if action fails.
         self.kTimeoutMs = 10
         
 		# Sets the speed
@@ -50,6 +23,7 @@ class Robot(wpilib.TimedRobot):
         self.ySpeed = 1
         self.tSpeed = 0.75
         
+        # Stores turn and movement axis
         self.yAxis = 0
         self.tAxis = 0
 
@@ -62,83 +36,77 @@ class Robot(wpilib.TimedRobot):
         # Create a simple timer (docs: https://robotpy.readthedocs.io/projects/wpilib/en/latest/wpilib/Timer.html#wpilib.timer.Timer.get)
         self.timer = wpilib.Timer()
         
-        self._state = State(self, FREEZE)
-        
+        # Talon CAN devices
         self.frontLeftTalon = WPI_TalonSRX(2)
         self.rearLeftTalon = WPI_TalonSRX(0)
         self.frontRightTalon = WPI_TalonSRX(3)
         self.rearRightTalon = WPI_TalonSRX(1)
 
+        # Enable auto breaking
         self.frontLeftTalon.setNeutralMode(NeutralMode.Brake)
         self.rearLeftTalon.setNeutralMode(NeutralMode.Brake)
         self.frontRightTalon.setNeutralMode(NeutralMode.Brake)
         self.rearRightTalon.setNeutralMode(NeutralMode.Brake)
 
+        # Setup encoders
         self.frontLeftTalon.configSelectedFeedbackSensor(
-            WPI_TalonSRX.FeedbackDevice.CTRE_MagEncoder_Relative,
+            FeedbackDevice.CTRE_MagEncoder_Relative,
             self.kPIDLoopIdx,
             self.kTimeoutMs,
         )
 
         self.rearLeftTalon.configSelectedFeedbackSensor(
-            WPI_TalonSRX.FeedbackDevice.CTRE_MagEncoder_Relative,
+            FeedbackDevice.CTRE_MagEncoder_Relative,
             self.kPIDLoopIdx,
             self.kTimeoutMs,
         )
 
         self.frontRightTalon.configSelectedFeedbackSensor(
-            WPI_TalonSRX.FeedbackDevice.CTRE_MagEncoder_Relative,
+            FeedbackDevice.CTRE_MagEncoder_Relative,
             self.kPIDLoopIdx,
             self.kTimeoutMs,
         )
 
         self.rearRightTalon.configSelectedFeedbackSensor(
-            WPI_TalonSRX.FeedbackDevice.CTRE_MagEncoder_Relative,
+            FeedbackDevice.CTRE_MagEncoder_Relative,
             self.kPIDLoopIdx,
             self.kTimeoutMs,
         )
 
+        # Setup encoders
         self.leftEncoder = self.rearLeftTalon
         self.rightEncoder = self.rearRightTalon
 
-    @property
-    def state(self):
-        return self._state.state
-        
-    @state.setter
-    def state(self, new_state):
-        self._state.dispatch(new_state)
-        
     def autonomousInit(self):
         """Called only at the beginning of autonomous mode."""
-        self.timer.reset()
-        self.timer.start()
+        pass
 
     def autonomousPeriodic(self):
         """Called every 20ms in autonomous mode."""
-        self._state.update()
-
-        #if self.state == DRIVE_FORWARD_TWO_SEC:
-        #    self.myRobot.tankDrive(0.3, 0.3)
-        #elif self.state == FREEZE:
-        #    self.myRobot.tankDrive(0, 0)
+        pass
 
     def teleopInit(self):
         pass
 
-    def teleopPeriodic(self):      
-        self._state.update()
-
+    def teleopPeriodic(self):
+        # Get max speed
         self.speed = (-self.joystick.getRawAxis(3) + 1)/2
 
+        # Get turn and movement speeds
         self.tAxis = self.threshhold(self.joystick.getRawAxis(2), 0.05) * self.tSpeed * self.speed
         self.yAxis = self.threshhold(-self.joystick.getRawAxis(1), 0.05) * self.ySpeed * self.speed
         
-        self.frontLeftTalon.set(WPI_TalonSRX.ControlMode.PercentOutput, self.yAxis+self.tAxis)
-        self.rearLeftTalon.set(WPI_TalonSRX.ControlMode.PercentOutput, self.yAxis+self.tAxis)
-        self.frontRightTalon.set(WPI_TalonSRX.ControlMode.PercentOutput, self.yAxis-self.tAxis)
-        self.rearRightTalon.set(WPI_TalonSRX.ControlMode.PercentOutput, self.yAxis-self.tAxis)
+        # Calculate right and left speeds
+        leftSpeed = self.yAxis+self.tAxis
+        rightSpeed = self.yAxis-self.tAxis
 
+        # Update Motors
+        self.frontLeftTalon.set(ControlMode.PercentOutput, leftSpeed)
+        self.rearLeftTalon.set(ControlMode.PercentOutput, leftSpeed)
+        self.frontRightTalon.set(ControlMode.PercentOutput, rightSpeed)
+        self.rearRightTalon.set(ControlMode.PercentOutput, rightSpeed)
+
+        # Update SmartDashboard
         self.sd.putNumber("Left Encoder", self.leftEncoder.getSelectedSensorPosition(self.kPIDLoopIdx))
         self.sd.putNumber("Right Encoder", self.rightEncoder.getSelectedSensorPosition(self.kPIDLoopIdx))
 
